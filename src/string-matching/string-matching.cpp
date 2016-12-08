5222feb2f021cbe1d8bd46ba5b2a9a6cf612963d
Michael Wray <Michael.Wray@bristol.ac.uk>
2016-12-08 12:08:11 +0000

    Fixed Compilation on Linux

--- src/string-matching/string-matching.cpp
+++ src/string-matching/string-matching.cpp
@@ -20,6 +20,7 @@ int main(int argc, char *argv[])
 
     string line;
     size_t line_id;
+    unsigned int offset = 0;
     while (reader.getLine(line, line_id))
     {
         vector<string> tokens = tools::split(line, "->");
@@ -41,13 +42,13 @@ int main(int argc, char *argv[])
                 print(str, pattern, offset);
 
                 // 2) For each character the pattern. Loop.
-                for (/* WRITE ME! */)
+                for (;;/* WRITE ME! */)
                 {
                     // 3) Compare string to pattern [hint: str.at(j + offset) == pattern.at(j)]
                 }
 
                 // 4) Have you found the end of the pattern?
-                if (/* WRITE ME! */)
+                if (true/* WRITE ME! */)
                     cout << " IS A MATCH!" << endl << endl;
 
                 else
@@ -60,10 +61,9 @@ int main(int argc, char *argv[])
         {
             KMPPrefixTable table(pattern); // Note: pre-fix table is automatically created
 
-            unsigned int
-                n = str.length(),
-                m = pattern.length(),
-                i = 0;
+            unsigned int n = str.length();
+            unsigned int m = pattern.length();
+            unsigned int i = 0;
 
             /*
             * Note: In c++, the indices start in 0.
@@ -71,23 +71,23 @@ int main(int argc, char *argv[])
             */
 
             // for j = 0 upto n step 1 do (Note j = 0)
-            for (/* WRITE ME! */)
+            for (;;/* WRITE ME! */)
             {
                 // while i > 0 and P[i] != T[j] do.
-                while (/* WRITE ME! */)
+                while (false/* WRITE ME! */)
                 {
                     // Skip using prex table
                     i = table[i];
                 }
 
                 // if P[i] = T[j] then (Note P[i])
-                if (/* WRITE ME! */)
+                if (true/* WRITE ME! */)
                 {
                     // Next character matches
                 }
 
                 // if you reach end of pattern [hint: i == m]
-                if (/* WRITE ME! */)
+                if (true/* WRITE ME! */)
                 {
                     // OUTPUT(j - m + 1) (Note + 1)
                     // Look for next match
@@ -99,18 +99,17 @@ int main(int argc, char *argv[])
         {
             BMHTable table(pattern);
 
-            unsigned int
-                n = str.length(), // Length of the string
-                m = pattern.length(), // Length of the pattern
-                offset = 0, // Offset (position of the pattern)
-                i = m - 1, // Character to test in pattern
-                j = i + offset; // Character to test in string
+            unsigned int n = str.length(); // Length of the string
+            unsigned int m = pattern.length(); // Length of the pattern
+            unsigned int offset = 0; // Offset (position of the pattern)
+            unsigned int i = m - 1; // Character to test in pattern
+            unsigned int j = i + offset; // Character to test in string
 
             // 1) Repeat until we reach the end of the string [hint: offset + m < n]
-            while (/* WRITE ME! */)
+            while (false/* WRITE ME! */)
             {
                 // 2) Compare character {i} to character {j}
-                if (/* WRITE ME! */)
+                if (true/* WRITE ME! */)
                 {
                     // 3A) The character matches, test next one?
                     if (i < 0)
